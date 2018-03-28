ec1e31c311030052b9eeca78aa2e8f04a039382a
Mirek <miroslaw.ochodek@gmail.com>
2018-03-28 12:31:37 +0200

    Spliting classifiers to different files, adding new ones

--- common/storage.py
+++ common/storage.py
@@ -9,13 +9,17 @@ class FileStorageHandler(object):
     Handles storage and reading from temporary folder
     """
 
-    def __init__(self, tmp_dir_path):
+    def __init__(self, tmp_dir_path, output_dir_name="results"):
         self.logger = logging.getLogger('pyccflex.common.configuration.TempStorageHandler')
         self.path = tmp_dir_path
+        self.output_dir_name = output_dir_name
+        self.results_path = self.get_file_path(self.output_dir_name)
 
     def create_storage_dir(self):
         os.makedirs(self.path)
         self.logger.info("Creating storage folder {}".format(self.path))
+        os.makedirs(self.results_path)
+        self.logger.info("Creating results folder {}".format(self.results_path))
 
     def remove_storage_dir(self):
         shutil.rmtree(self.path)
@@ -23,4 +27,9 @@ class FileStorageHandler(object):
 
     def get_file_path(self, filename):
         return os.path.join(os.path.normpath(self.path), filename)
+
+    def get_results_file_path(self, filename):
+        return os.path.join(os.path.normpath(self.results_path), filename)
+
+
 
--- run.sh
+++ run.sh
@@ -4,13 +4,24 @@ LOCATIONS_CONFIG="./locations.json"
 CLASSES_CONFIG="./classes.json"
 FILES_FORMAT_CONFIG="./files_format.json"
 MANUAL_FEATURES_CONFIG="./manual_features.json"
+CLASSIFIERS_CONFIG="./classifiers_options.json"
 
+# create storage
 create-storage --locations_config $LOCATIONS_CONFIG
+
+# read codebases, transform them to CSV, and extract features
 lines2csv "train" --locations_config $LOCATIONS_CONFIG --classes_config $CLASSES_CONFIG --files_format_config $FILES_FORMAT_CONFIG
 basic-manual-features "train" --add_decision_class true --add_contents true --locations_config $LOCATIONS_CONFIG --manual_features_config $MANUAL_FEATURES_CONFIG
 lines2csv "classify" --locations_config $LOCATIONS_CONFIG --classes_config $CLASSES_CONFIG --files_format_config $FILES_FORMAT_CONFIG
 basic-manual-features "classify" --add_decision_class true --add_contents true --locations_config $LOCATIONS_CONFIG --manual_features_config $MANUAL_FEATURES_CONFIG
-classify "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG
+
+# run classification algorithms
+classify_CART "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG --classifiers_options $CLASSIFIERS_CONFIG --classes_config $CLASSES_CONFIG
+classify_RandomForest "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG --classifiers_options $CLASSIFIERS_CONFIG --classes_config $CLASSES_CONFIG
+classify_KNN "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG --classifiers_options $CLASSIFIERS_CONFIG --classes_config $CLASSES_CONFIG
+classify_MultinomialNB "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG --classifiers_options $CLASSIFIERS_CONFIG --classes_config $CLASSES_CONFIG
 classify_c50_r "train-basic-manual.csv" "classify-basic-manual.csv" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG
+
+# generate html reports
 generate_html "train-basic-manual.csv" "training-lines-html.html" --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG
 generate_html "classify-output.csv" "classified-lines-html.html" --all OK --locations_config $LOCATIONS_CONFIG --files_format_config $FILES_FORMAT_CONFIG
