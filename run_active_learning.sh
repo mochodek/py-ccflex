c27505c3de30d20edc353b860bf90ae91bff8516
Mirek <miroslaw.ochodek@gmail.com>
2018-05-11 08:44:35 +0200

    Adding --committee options that uses committee-based active learning

--- run_active_learning.sh
+++ run_active_learning.sh
@@ -7,10 +7,11 @@ MANUAL_FEATURES_CONFIG="./manual_features.json"
 CLASSIFIERS_CONFIG="./classifiers_options.json"
 FEATURE_SELECTORS_CONFIG="./feature_selectors_options.json"
 
-active_learning  --input_files "train-features.csv" "classify-features.csv" \
+active_learning  --input_files "train-manual-bow.csv" "classify-manual-bow.csv" \
     --output_file "active_learning_input.csv" \
     --max_lines 500000 --use_existing_labels  \
     --base_learner "RandomForest" \
+    --committee \
     --add_contents \
     --locations_config $LOCATIONS_CONFIG \
     --files_format_config $FILES_FORMAT_CONFIG \
